617a8df536d22d60f9dc019b7c35013e4425a2c3
yuanlaiyl <498893986@qq.com>
2019-11-25 22:34:37 -0800

    add week12

---
+++ week12/my.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
