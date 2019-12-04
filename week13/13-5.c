b84554bb343147a687284d046c0f128c39ae5b93
yuanlaiyl <498893986@qq.com>
2019-12-04 01:01:34 -0800

    add 4 files

---
+++ week13/13-5.c
@@ -0,0 +1,27 @@
+#include "my.h"
+
+void sig(int signo,siginfo_t *info,void *context)
+{
+   printf("\nget signal :%d\n",signo);
+   printf("signal number is %d\n",info->si_signo);
+   printf("sender pid %d\n",info->si_pid);
+   printf("sigval=%d \n",info->si_value.sival_int);
+}
+
+int main()
+{
+   struct sigaction new;
+   sigemptyset(&new.sa_mask);
+   new.sa_sigaction=sig;
+   new.sa_flags|=SA_SIGINFO|SA_RESTART;
+  
+   if(sigaction(36,&new,NULL)==-1)
+   {
+     printf("set signal process mode.\n");
+
+   }
+   while(1)
+      pause();
+   printf("Done.\n");
+   exit(0);
+}
